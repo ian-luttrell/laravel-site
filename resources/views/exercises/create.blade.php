3d7dd60eec4647d112f0b20be95989e6d4a9ca25
Ian Luttrell <ian.j.luttrell@gmail.com>
2019-03-16 17:40:18 -0700

    add style to tables

--- resources/views/exercises/create.blade.php
+++ resources/views/exercises/create.blade.php
@@ -45,12 +45,12 @@ Exercise Tracker: Record Exercise
 		<th>Minutes</th>
 	</tr>
 
-	@foreach ($userExercises as $exercise)
+	@foreach ($userExercises as $exercise)			
 		<tr>
-			<td>{{ $exercise->date }}</td>
-			<td>{{ $exercise->distance }}</td>
-			<td>{{ $exercise->hours }}</td>
-			<td>{{ $exercise->minutes }}</td>
+			<td><a class="table-link" href="/">{{ $exercise->date }}</a></td>
+			<td><a class="table-link" href="/">{{ $exercise->distance }}</a></td>
+			<td><a class="table-link" href="/">{{ $exercise->hours }}</a></td>
+			<td><a class="table-link" href="/">{{ $exercise->minutes }}</a></td>
     	</tr>
 	@endforeach
 
