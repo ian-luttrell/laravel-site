50ef81b209e5566f3371a10e8c762d8ccb3c5c1b
Ian Luttrell <ian.j.luttrell@gmail.com>
2019-03-17 15:22:48 -0700

    add show() method to exercise resource

--- resources/views/exercises/index.blade.php
+++ resources/views/exercises/index.blade.php
@@ -1,44 +1,51 @@
 @extends('layouts.template')
 
+
 @section('title')
+
 Exercise Tracker: Record Exercise
+
 @endsection
+
 
 @section('content')
 
-<a class="plain-link" href="/exercises/create"><span class="link-button">Add New Exercise</span></a>
 
-<br><br><br>
+	<a class="plain-link" href="/exercises/create"><span class="link-button">Add New Exercise</span></a>
 
-@if ($userExercises->count() > 0)
+	<br><br><br>
 
-	<table>
+	@if ($userExercises->count() > 0)
 
-		<tr>
-			<th>Date</th>
-			<th>Location</th>
-			<th>Distance (miles)</th>
-			<th>Hours</th>
-			<th>Minutes</th>
-		</tr>
+		<table>
 
-		@foreach ($userExercises as $exercise)			
 			<tr>
-				<td><a class="table-link" href= {{ url('/exercises/'.$exercise->id) }} >{{ $exercise->date }}</a></td>
-				<td><a class="table-link" href="/">{{ $exercise->location }}</a></td>
-				<td><a class="table-link" href="/">{{ $exercise->distance }}</a></td>
-				<td><a class="table-link" href="/">{{ $exercise->hours }}</a></td>
-				<td><a class="table-link" href="/">{{ $exercise->minutes }}</a></td>
+				<th>Date</th>
+				<th>Location</th>
+				<th>Distance (miles)</th>
+				<th>Hours</th>
+				<th>Minutes</th>
 			</tr>
-		@endforeach
 
+			@foreach ($userExercises as $exercise)
+			
+				<tr>
+					<td><a class="table-link" href= {{ url('/exercises/'.$exercise->id) }} > {{ $exercise->date }} </a></td>
+					<td><a class="table-link" href= {{ url('/exercises/'.$exercise->id) }} > {{ $exercise->location }} </a></td>
+					<td><a class="table-link" href= {{ url('/exercises/'.$exercise->id) }} > {{ $exercise->distance }} </a></td>
+					<td><a class="table-link" href= {{ url('/exercises/'.$exercise->id) }} > {{ $exercise->hours }} </a></td>
+					<td><a class="table-link" href= {{ url('/exercises/'.$exercise->id) }} > {{ $exercise->minutes }} </a></td>
+				</tr>
 
-	</table>
+			@endforeach
 
-@else
+		</table>
 
-	You have not recorded any exercises yet.
+	@else
 
-@endif
+		You have not recorded any exercises yet.
+
+	@endif
+
 
 @endsection
